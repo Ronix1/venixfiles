995242ea0ae622a193bf974e9ee0942b4cd379ab
Ronix1 <165508755+Ronix1@users.noreply.github.com>
2025-07-25 20:51:59 +0300

    Add files via upload

---
+++ devices.ps1
@@ -0,0 +1,20 @@
+Get-PnpDevice -FriendlyName "*Composite Bus Enumerator*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Intel Management Engine*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Intel SPI (flash) Controller*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Microsoft GS Wavetable Synth*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*NDIS Virtual Network Adapter Enumerator*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Remote Desktop Device Redirector Bus*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*SMBus*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*UMBus*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*UMBus Root Bus Enumerator*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*System Timer*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*NDIS Virtual Network Adapter Enumerator*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Composite Bus Enumerator*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Programmable interrupt controller*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Numeric data processor*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*High Precision event timer*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Intel SPI*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Microsoft GS Wavetable Synth*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Microsoft Hyper-V-Compliant System*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Microsoft System Management BIOS Driver*" | Disable-PnpDevice -Confirm:$false
+Get-PnpDevice -FriendlyName "*Remote Desktop Device Redirector Bus*" | Disable-PnpDevice -Confirm:$false
